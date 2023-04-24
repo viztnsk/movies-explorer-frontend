f485085bfa98ca84f415993d4b223b804e3991d1
Victoria Zatonskaya <viztnsk@yandex.ru>
2023-04-24 17:14:19 +0300

    review fixes – semantics and positioning

--- movies-explorer-frontend/src/components/Portfolio/Portfolio.jsx
+++ movies-explorer-frontend/src/components/Portfolio/Portfolio.jsx
@@ -5,20 +5,26 @@ function Portfolio() {
       <h4 className="portfolio__heading">
       Портфолио
       </h4>
-      <div className='portfolio__container'>
-        <div className='portfolio__links'>
-          <Link to='https://viztnsk.github.io/how-to-learn/' className='portfolio__link' target="_blank">Статичный сайт</Link>
-          <p className="portfolio__arrow">↗</p>
-        </div>
-        <div className='portfolio__links'>
-          <Link to='https://viztnsk.github.io/russian-travel/' className='portfolio__link' target="_blank">Адаптивный сайт</Link>
-          <p className="portfolio__arrow">↗</p>
-        </div>
-        <div className='portfolio__links'>
-          <Link to='https://viztnsk.mesto.nomoredomains.work/' className='portfolio__link' target="_blank">Одностраничное приложение</Link>
-          <p className="portfolio__arrow">↗</p>
-        </div>
-      </div>
+      <ul className='portfolio__links'>
+          <li className="portfolio__link">
+            <Link to='https://viztnsk.github.io/how-to-learn/' className='link' target="_blank">
+            <p className="link__text">Статичный сайт</p>
+            <p className="link__arrow">↗</p>
+            </Link>
+          </li>
+          <li className="portfolio__link">
+            <Link to='https://viztnsk.github.io/russian-travel/' className='link' target="_blank">
+            <p className="link__text">Адаптивный сайт</p>
+            <p className="link__arrow">↗</p>
+            </Link>
+          </li>
+          <li className="portfolio__link">
+            <Link to='https://viztnsk.mesto.nomoredomains.work/' className='link' target="_blank">
+              <p className="link__text">Одностраничное приложение</p>
+              <p className="link__arrow">↗</p>
+            </Link>
+          </li>
+      </ul>
     </section>
   )
 }
--- movies-explorer-frontend/src/components/SearchForm/SearchForm.jsx
+++ movies-explorer-frontend/src/components/SearchForm/SearchForm.jsx
@@ -4,7 +4,7 @@ function SearchForm() {
   return (
     <section className="search">
       <form  className='search__form' name='movie' action="">
-        <input className='search__input' type="text" placeholder='Фильм'/>
+        <input className='search__input' type="text" placeholder='Фильм' required/>
         <button className="search__button" type='submit'></button>
         <FilterCheckbox></FilterCheckbox> 
       </form>
